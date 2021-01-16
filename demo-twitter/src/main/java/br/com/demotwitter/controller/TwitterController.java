81d94b803e1f4c4b4ff6f74fc27bd53e1d7a2e8b
Andrew Sato <andrewsato10@gmail.com>
2021-01-15 22:38:20 -0300

    remoção de arquivos de testes e adição de verificação se a requisição vem um client id

--- demo-twitter/src/main/java/br/com/demotwitter/controller/TwitterController.java
+++ demo-twitter/src/main/java/br/com/demotwitter/controller/TwitterController.java
@@ -1,23 +1,43 @@
 package br.com.demotwitter.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.demotwitter.model.DemoTwitterResponseDto;
 import br.com.demotwitter.service.TwitterService;
 
 @RestController
 public class TwitterController {
+
+	private HttpServletRequest request;
+	//client id para chamar o controller
+	static final String clientId = "4c0a20945ff3429ccefd66d63d310a4c";
 	
 	@Autowired
 	TwitterService twitterService;
-	
+
+	@Autowired
+	public TwitterController(HttpServletRequest request) {
+		this.request = request;
+	}
+
 	@GetMapping("/user/{username}")
-    public DemoTwitterResponseDto getTopHashtagFromUser(@PathVariable("username") String username) {
+	public DemoTwitterResponseDto getTopHashtagFromUser(@PathVariable("username") String username) {
+		
 		String user = twitterService.getUsernameId(username);
-		return twitterService.getTopHashtagFromUser(user);
-    }
+		String userClientId = request.getHeader("client-id");
+		
+		if (userClientId != null && userClientId.equals(clientId)) {
+			return twitterService.getTopHashtagFromUser(user);
+		}
+		throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+
+	}
 
 }
